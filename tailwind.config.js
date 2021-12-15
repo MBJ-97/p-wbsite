d40d4585523bfd5111c0207b353ea37e8927c4c5
MBJ-97 <mehdiibeldjoudi@gmail.com>
2021-12-15 11:34:17 +0100

    changes uplaoded

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       'epilogue': ['Epilogue', 'sans-serif']
     },
     extend: {
+      dropShadow: {
+        '3xl': '0 25px 25px rgb(255,255,255)',      
+        },
       colors: {
         primary: "#BFEAE0",
         secondary: "#EE671F"
